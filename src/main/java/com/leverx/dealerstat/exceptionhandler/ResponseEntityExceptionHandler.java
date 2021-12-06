e8a361e41ceb7999aa6db923887d38c7107e822c
= <=>
2021-12-06 17:52:49 +0300

    Created exception handler for 5 exceptions

---
+++ src/main/java/com/leverx/dealerstat/exceptionhandler/ResponseEntityExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.leverx.dealerstat.exceptionhandler;
+
+import com.leverx.dealerstat.exception.*;
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({NotFoundException.class})
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({AlreadyExistsException.class})
+    public ResponseEntity<?> handleAlreadyExistsException(AlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler({JwtAuthenticationException.class, JwtException.class, UsernameNotFoundException.class})
+    public ResponseEntity<?> handleJwtAuthenticationException(JwtAuthenticationException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({MessageSendingException.class})
+    public ResponseEntity<?> handleMessageSendingException(MessageSendingException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
