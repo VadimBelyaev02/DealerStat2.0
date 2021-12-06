3d819b7c19230c2329c2953c0e1e6258c4dbc019
= <=>
2021-12-06 15:10:55 +0300

    Refactoring DTO

--- src/main/java/com/leverx/dealerstat/dto/GameDTO.java
+++ src/main/java/com/leverx/dealerstat/dto/GameDTO.java
@@ -1,10 +1,14 @@
 package com.leverx.dealerstat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameDTO {
 
     private Long id;
--- src/main/java/com/leverx/dealerstat/dto/UserDTO.java
+++ src/main/java/com/leverx/dealerstat/dto/UserDTO.java
@@ -1,15 +1,23 @@
 package com.leverx.dealerstat.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.leverx.dealerstat.model.Role;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
     private String firstName;
     private String lastName;
+  //  @JsonIgnore
     private String password;
     private String email;
-
 }
