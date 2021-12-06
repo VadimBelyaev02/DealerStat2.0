eef607d518775c86b0b9654d452df6005cc63f71
= <=>
2021-12-06 15:18:09 +0300

    Refactoring security

---
+++ src/main/java/com/leverx/dealerstat/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.leverx.dealerstat.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
