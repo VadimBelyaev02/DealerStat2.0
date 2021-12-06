4f74f16bd1d91baaffc5d3d3f633d335b9956ac7
= <=>
2021-12-07 00:12:57 +0300

    Refactoring

--- src/main/java/com/leverx/dealerstat/security/AuthenticatedUserFactory.java
+++ src/main/java/com/leverx/dealerstat/security/AuthenticatedUserFactory.java
@@ -1,6 +1,6 @@
 package com.leverx.dealerstat.security;
 
-import com.leverx.dealerstat.converter.UsersConverter;
+import com.leverx.dealerstat.converter.UserConverter;
 import com.leverx.dealerstat.dto.UserDTO;
 import com.leverx.dealerstat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +13,16 @@ import org.springframework.stereotype.Component;
 public class AuthenticatedUserFactory {
 
     private final UserService userService;
-    private final UsersConverter usersConverter;
 
     @Autowired
-    public AuthenticatedUserFactory(UserService userService, UsersConverter usersConverter) {
+    public AuthenticatedUserFactory(UserService userService) {
         this.userService = userService;
-        this.usersConverter = usersConverter;
     }
 
     public UserDTO currentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String email = userDetails.getUsername();
-        return usersConverter.convertToDTO(userService.findByEmail(email));
+        return userService.findByEmail(email);
     }
 }
